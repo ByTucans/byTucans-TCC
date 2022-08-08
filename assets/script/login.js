4bc9f2f6d566d9a395202c2017875845bbeb044d
Daniel S Filho <105764608+ByTucans@users.noreply.github.com>
2022-08-08 20:42:12 -0300

    corte do maracana

---
+++ assets/script/login.js
@@ -0,0 +1,32 @@
+function loadPage(){
+    exibirPassword();
+}
+
+function exibirPassword (){
+    let container = document.querySelector(".box-password");
+    let input = document.querySelector(".senha");
+    let eye = document.querySelector(".eye");
+
+    eye.addEventListener('click', function() {
+        container.classList.toggle('visible');
+        if(container.classList.contains('visible')){
+            eye.src = 'assets/image/eye.svg';
+            input.type = 'text';
+        }else{
+            eye.src = 'assets/image/eye-off.svg';
+            input.type = 'password';
+        }
+    });
+}
+
+function eyePassword(){
+    let input = document.querySelector(".senha");
+    let eye = document.querySelector(".eye");
+
+    if(input.value != ''){  
+        eye.style.display = 'inline-block';
+    }else{
+    eye.style.display = 'none';
+    }  
+}
+
